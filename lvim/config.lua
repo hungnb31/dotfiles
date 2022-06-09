61c1773d25d170f21b525793e4f4eea389a1d04a
Howard <hungnb31@gmail.com>
2022-06-09 16:41:32 +0700

    feat: updated lvim config

--- lvim/config.lua
+++ lvim/config.lua
@@ -29,6 +29,15 @@ lvim.builtin.which_key.mappings["s"] = {
   j = { ":split<cr>", "Horizontal Split" },
   l = { ":vsplit<cr>", "Vertical Split" },
 }
+
+lvim.builtin.which_key.mappings["h"] = {
+  "<cmd>nohlsearch<cr>", "No Highlight"
+}
+
+lvim.builtin.which_key.mappings["n"] = {
+  "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover"
+}
+
 lvim.builtin.which_key.mappings["f"] = {
   name = "Find",
   b = { "<cmd>Telescope git_branches<cr>", "Checkout Branch" },
@@ -128,11 +137,12 @@ lvim.keys.normal_mode = {
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = true
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.show_icons.git = 0
-lvim.builtin.nvimtree.setup.view.auto_resize = true
+lvim.builtin.nvimtree.setup.auto_reload_on_write = true
+lvim.builtin.nvimtree.setup.actions.open_file.resize_window = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
